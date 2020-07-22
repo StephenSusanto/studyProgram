7aa0875005ce38bc71fccc5938cf8ab9cd811b2b
StephenSusanto <53929056+StephenSusanto@users.noreply.github.com>
2020-07-22 15:05:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title> Belajar Coding PHP Native </title>
+    </head>
+    <body>
+    <center>
+        <h1>Tabel Data </h1>
+            <div>
+                <table border ="1">
+                    <tr>
+                        <th>Nomor </th>
+                        <th>Nama </th>
+                        <th>Tanggal Lahir</th>
+                    </tr>
+                </table>
+            </div>
+        </center>
+    </body>
+</html>
